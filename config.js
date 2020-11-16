54d690368a4ec485398d03555cf144f682bed9dd
Sampli <52607241+samplics@users.noreply.github.com>
2020-11-16 13:21:04 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    cookie: {
+        ogusersmybbuser: "",
+        oguserssid: "",
+        ogusersupgrade56: ""
+    },
+    thread_name: "",
+    post_key: "",
+    post_hash: ""
+}
